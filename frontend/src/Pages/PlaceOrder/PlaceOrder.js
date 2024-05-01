852db6582a4a67317e748c46a724f761193ce8a5
Nilophar <nilophar@helpabless.org>
2024-05-01 13:23:23 +0530

    implementing routing

---
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./PlaceOrder"
+export const PlaceOrder = () => {
+  return (
+    <div>placeorder</div>
+  )
+}
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import ExploreMenu from "../ExploreMenu/ExploreMenu";
 export default function Navbar() {
+  const [menu, setMenu] = useState("Home");
   return (
-    // <ul>
-    //   <li>
-    //     <a class="active" href="#home">
-    //       Home
-    //     </a>
-    //   </li>
-    //   <li>
-    //     <a href="#news">Recipes</a>
-    //   </li>
-    //   <li>
-    //     <a href="#contact">Settings</a>
-    //   </li>
-    // </ul>
     <div class="Navbar">
       <img src={assets.logo} alt="" className="logo" />
       <img src={assets.logout_icon} alt="" className="logout_icon" />
-      <img src={assets.menu_1} alt="" className="menu_1" />
-      <img src={assets.menu_2} alt="" className="menu_2" />
+      <img src={assets.header_img} alt="" className="header_img" />
+      <img src={assets.rating_starts} alt="" className=" rating_starts" />
+      <img src={assets.add_icon_green} alt="" className="add_icon_green" />
+      <img src={assets.add_icon_white} alt="" className="add_icon_white" />
+      <img src={assets.remove_icon_red} alt="" className="remove_icon_red" />
+      <img src={assets.app_store} alt="" className="app_store" />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Setting</li>
-        <li>Contact-us</li>
+        <li onClick={() => "Home"} className={menu === "Home" ? "active" : ""}>
+          Home
+        </li>
+        <li onClick={() => "Menu"} className={menu === "Menu" ? "active" : ""}>
+          Menu
+        </li>
+        <li
+          onClick={() => "Setting"}
+          className={menu === "Setting" ? "active" : ""}
+        >
+          Setting
+        </li>
+        <li
+          onClick={() => "Contact-us"}
+          className={menu === "Contact-us" ? "active" : ""}
+        >
+          Contact-us
+        </li>
       </ul>
-      <div className="=navbar-right">
-        <ExploreMenu />
-      </div>
+      <div className="navbar-right" />
+      <img src={assets.search_icon} alt="" />
+      <div className="navbar-search-icon" />
+      <img src={assets.basket_icon} alt="" />
+      <div className="dot" />   
+      <button>sign in</button>
+      <ExploreMenu />;
     </div>
   );
 }
